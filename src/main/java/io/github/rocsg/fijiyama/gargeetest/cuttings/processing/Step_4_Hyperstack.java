28993af1db75427e7a4576b3d06625fb066eb4a0
Gargee Phukon <gargee161011@example.com>
2024-12-20 17:30:57 +0100

    Gargee Fork Update: Refactoring in progress

--- src/main/java/io/github/rocsg/fijiyama/gargeetest/cuttings/processing/Step_4_Hyperstack.java
+++ src/main/java/io/github/rocsg/fijiyama/gargeetest/cuttings/processing/Step_4_Hyperstack.java
@@ -12,6 +12,8 @@ import io.github.rocsg.fijiyama.gargeetest.cuttings.core.Specimen;
 
 public class Step_4_Hyperstack implements PipelineStep{
     public void execute(Specimen specimen, boolean testing) throws Exception {
+        Specimen spec= new Specimen("B_201");
+        new Step_4_Hyperstack().execute(spec,true); 
         ImagePlus hyperStack=null;
         String dir="";
         //Test if files are available to reach step 4
@@ -43,7 +45,7 @@ public class Step_4_Hyperstack implements PipelineStep{
             
                     else{
                         //compute level 0 hyperstack (all the raws)
-                        
+                        createHyperStacks(spec);
                     }
                         
                 }
@@ -63,17 +65,17 @@ public class Step_4_Hyperstack implements PipelineStep{
     // }
 
 
-    public static ImagePlus createHyperStacks(String specimen) {
-        ImagePlus imgB001 = IJ.openImage(mainDir + specimen + "/raw/" + specimen + "_J001_aligned.tif");
-        ImagePlus imgB029 = IJ.openImage(mainDir + specimen + "/outputs/" + specimen + "_001_029_TransformedImage_Reg_Aligned.tif");
-        ImagePlus imgB077 = IJ.openImage(mainDir + specimen + "/outputs/" + specimen + "_001_077_TransformedImage_Reg_Aligned.tif");
-        ImagePlus imgB141 = IJ.openImage(mainDir + specimen + "/outputs/" + specimen + "_001_0141_TransformedImage_Reg_Aligned.tif");
+    public static ImagePlus createHyperStacks(Specimen specimen) {
+        ImagePlus imgB001 = IJ.openImage(Config.mainDir + specimen + "/raw/" + specimen + "_J001_aligned.tif");
+        ImagePlus imgB029 = IJ.openImage(Config.mainDir + specimen + "/outputs/" + specimen + "_001_029_TransformedImage_Reg_Aligned.tif");
+        ImagePlus imgB077 = IJ.openImage(Config.mainDir + specimen + "/outputs/" + specimen + "_001_077_TransformedImage_Reg_Aligned.tif");
+        ImagePlus imgB141 = IJ.openImage(Config.mainDir + specimen + "/outputs/" + specimen + "_001_0141_TransformedImage_Reg_Aligned.tif");
         ImagePlus[] tabImgover4setsofdays = new ImagePlus[] { imgB001, imgB029, imgB077, imgB141 };
         ImagePlus hyperStackXR = VitimageUtils.hyperStackingChannels(tabImgover4setsofdays);
         ImagePlus hyperFrame = VitimageUtils.hyperStackChannelToHyperStackFrame(hyperStackXR);
         // hyperFrame.setTitle(specimen+"_Hyperstack");
         // hyperFrame.show();
-        IJ.saveAsTiff(hyperFrame, mainDir + specimen + "/hyperimage/" + specimen + "_Hyperstack.tif");
+        IJ.saveAsTiff(hyperFrame, Config.mainDir + specimen + "/hyperimage/" + specimen + "_Hyperstack.tif");
         return hyperFrame;
     }
 
